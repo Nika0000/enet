59dfd967f40b3be8b20f0eabc4e63fc6b12a2dfc
Nika0000 <nikainasaridze99@icloud.com>
2025-06-29 20:39:07 +0400

    feat: migrate to hooks as native_assets_cli was replaced

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.0
+
+* Migrate to hooks
+
 ## 0.0.1
 
 * Initial Release
--- hook/build.dart
+++ hook/build.dart
@@ -5,51 +5,42 @@ import 'package:native_toolchain_c/native_toolchain_c.dart';
 void main(List<String> args) async {
   const isDebugMode = bool.fromEnvironment('ENET_DEBUG');
 
-  await build(
-    args,
-    (input, output) async {
-      final packageName = input.packageName;
-      final targetOS = input.config.code.targetOS;
-      final linkMode = input.config.code.linkModePreference;
+  await build(args, (input, output) async {
+    final packageName = input.packageName;
+    final targetOS = input.config.code.targetOS;
+    final linkMode = input.config.code.linkModePreference;
 
-      final flags = <String>[];
-      final defines = <String, String>{};
+    final flags = <String>[];
+    final defines = <String, String>{};
 
-      if (isDebugMode) {
-        defines['ENET_DEBUG'] = '1';
+    if (isDebugMode) {
+      defines['ENET_DEBUG'] = '1';
 
-        // link log lib for android
-        if (targetOS == OS.android) {
-          flags.add('-llog');
-        }
+      // link log lib for android
+      if (targetOS == OS.android) {
+        flags.add('-llog');
       }
+    }
 
-      if (targetOS == OS.windows) {
-        flags.add('/W3'); // Equivalent to MSVC /W3 warning level
-        if (linkMode == LinkModePreference.dynamic) {
-          defines['ENET_DLL'] = '0';
-        } else {
-          defines['ENET_DLL'] = '1';
-        }
-      } else if (targetOS == OS.linux) {
-        flags.add('-Wall'); // Common flag for Linux GCC/Clang compilers
+    if (targetOS == OS.windows) {
+      flags.add('/W3'); // Equivalent to MSVC /W3 warning level
+      if (linkMode == LinkModePreference.dynamic) {
+        defines['ENET_DLL'] = '0';
+      } else {
+        defines['ENET_DLL'] = '1';
       }
+    } else if (targetOS == OS.linux) {
+      flags.add('-Wall'); // Common flag for Linux GCC/Clang compilers
+    }
 
-      final cbuilder = CBuilder.library(
-        name: packageName,
-        assetName: '$packageName.dart',
-        flags: flags,
-        defines: defines,
-        sources: [
-          'src/$packageName.c',
-        ],
-      );
+    final cbuilder = CBuilder.library(
+      name: packageName,
+      assetName: '$packageName.dart',
+      flags: flags,
+      defines: defines,
+      sources: ['src/$packageName.c'],
+    );
 
-      await cbuilder.run(
-        input: input,
-        output: output,
-        logger: null,
-      );
-    },
-  );
+    await cbuilder.run(input: input, output: output, logger: null);
+  });
 }
